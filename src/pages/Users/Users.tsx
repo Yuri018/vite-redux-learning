bad651551a4a3227a4ed8a5883738a19a9508bce
Juri Ryzhkov <plktmn@gmail.com>
2023-12-18 20:46:22 +0100

    add Homework 32

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,8 +1,22 @@
-import styled from "styled-components";
+import { useSelector } from "react-redux"
+import { usersState } from "store/redux/Users/selectors"
+
+import { UsersCardWrapper, UserCard, Titles } from "./styles"
 
 function Users() {
-    // Тут нужно получить данные со стора о users и показать данные в виде карточек
-    return <>Users</>
+  const usersData = useSelector(usersState)
+
+  return (
+    <UsersCardWrapper>
+      {usersData.users.map((user) => (
+        <UserCard>
+          <Titles>Name: {user.firstLastName}</Titles>
+          <Titles>Age: {user.age}</Titles>
+          <Titles>Job: {user.jobTitle}</Titles>
+        </UserCard>
+      ))}
+    </UsersCardWrapper>
+  )
 }
 
-export default Users;
+export default Users
--- src/pages/Users/styles.ts
+++ src/pages/Users/styles.ts
@@ -1,1 +1,23 @@
-import styled from "styled-components";
+import styled from "styled-components"
+
+export const UsersCardWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap;
+  gap: 20px;
+  align-content: flex-start;
+`
+
+export const UserCard = styled.div`
+  width: 23%;
+  height: fit-content;
+  min-width: 200px;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid black;
+`
+
+export const Titles = styled.p`
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+`
